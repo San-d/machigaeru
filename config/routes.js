2cbff2482c83b3747105f4d29ff6ec98955faa11
SanIsHereForYou <sdhar@tikkatech.com>
2015-08-27 16:10:20 +0530

    nodemodules added

---
+++ config/routes.js
@@ -0,0 +1,23 @@
+module.exports.routes = {
+
+  /***************************************************************************
+  *                                                                          *
+  * Make the view located at `views/homepage.ejs` (or `views/homepage.jade`, *
+  * etc. depending on your default view engine) your home page.              *
+  *                                                                          *
+  * (Alternatively, remove this and add an `index.html` file in your         *
+  * `assets` directory)                                                      *
+  *                                                                          *
+  ***************************************************************************/
+
+  '/': {
+    view: 'homepage'
+  },
+  /* Users */
+  'post /login' : 'UsersController.login',
+  'post /create/user' : 'UsersController.create_user',
+  'post /user/profile' : 'UsersController.user_profile',
+  'post /finduser' : 'UsersController.find_user',
+  'post /create/verificationcode' : 'UsersController.create_verification_code',
+  'post /verify/mobileno' : 'UsersController.verify_code'
+};
